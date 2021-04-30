a3f30c365253c81a9b6a8ad6344aefbbbb757326
Silverfoxy <baminazad@cs.stonybrook.edu>
2021-04-30 17:45:02 -0400

    Adding new db logging parameters (branch file/line)

--- php-emul/emulator.php
+++ php-emul/emulator.php
@@ -99,6 +99,8 @@ class Emulator
 
     public array $ast;
 
+    public $execution_id = 0; // Used in the distributed version
+
     public array $initenv;
     public string $httpverb;
     public string $entry_file;
@@ -1640,7 +1642,7 @@ class Emulator
             $this->verbose(sprintf('Adding reanimation task at %s:%d', $this->current_file, $this->current_line).PHP_EOL);
             // $this->verbose(print_r($this->full_reanimation_transcript, true).PHP_EOL);
             // $this->verbose('Line coverage hash: '.md5(json_encode($this->lineLogger->coverage_info)).PHP_EOL);
-            $this->reanimation_callback_object->add_reanimation_task($this->initenv, $this->httpverb, $this->entry_file, $this->full_reanimation_transcript, $this->current_line, $md5_current_line_coverage_state, $md5_current_state, $this->lineLogger->coverage_info);
+            $this->reanimation_callback_object->add_reanimation_task($this->initenv, $this->httpverb, $this->entry_file, $this->full_reanimation_transcript, $this->current_file, $this->current_line, $md5_current_line_coverage_state, $md5_current_state, $this->lineLogger->coverage_info, $this->execution_id);
             return array($pid, $child_pid);
         }
     }
