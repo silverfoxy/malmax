17030eacb63990816af992e6db9cc39b7705e85c
Silverfoxy <baminazad@cs.stonybrook.edu>
2022-02-18 00:35:10 -0500

    Fix Symbolic variable default concrete_values

--- php-emul/SymbolicVariable.php
+++ php-emul/SymbolicVariable.php
@@ -14,7 +14,7 @@ class SymbolicVariable
     public $type;
     public array $concrete_values = [];
 
-    public function __construct($variable_name='',$variable_value = '*', $type=NodeAbstract::class, $isset=null, array $concrete_values=[])
+    public function __construct($variable_name='',$variable_value = '*', $type=NodeAbstract::class, $isset=null, ?array $concrete_values=[])
     {
         /*
          * For normal SymbolicVariables, isset also returns symbolic.
@@ -24,7 +24,7 @@ class SymbolicVariable
         $this->variable_name = $variable_name;
         $this->variable_value = $variable_value;
         $this->type = $type;
-        $this->concrete_values = $concrete_values;
+        $this->concrete_values = $concrete_values ?? [];
     }
 
     public function __toString()
