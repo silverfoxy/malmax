d3fc3827dac4a05e3999b07ffc08613ec558470d
Tsoukaladelis <tsoukchris@gmail.com>
2022-06-14 15:45:52 +0000

    fixes

--- php-emul/emulator-statement.php
+++ php-emul/emulator-statement.php
@@ -113,8 +113,8 @@ trait EmulatorStatement
             $symbolic_iterations = $this->symbolic_loop_iterations;
             $expr_cond = $this->evaluate_expression($node->cond);
 			while ($expr_cond &&
-                   ($expr_cond instanceof SymbolicVariable ? $symbolic_iterations-- > 0 : true)
-                    && $expr_cond->symbolic_status===null)
+                   (($expr_cond instanceof SymbolicVariable ? $symbolic_iterations-- > 0 : true) &&  #if not symbolic, this is set to true, however:
+                   ($expr_cond->symbolic_status==true ? $symbolic_iterations-- > 0 : true))) #need to check if the object was created through symbolic variable call
 			{
 				$i++;
 				$this->run_code($node->stmts);
--- php-emul/mocks/mysqli_result_fetch_object.php
+++ php-emul/mocks/mysqli_result_fetch_object.php
@@ -2,7 +2,7 @@
 
 use PHPEmul\OOEmulator;
 
-function mysqli_result_fetch_object_mock($emul, $class, $constructor_args){
+function mysqli_result_fetch_object_mock($emul, $class, $constructor_args = []){
     $classname = strtolower($class->value->value);
     $class_obj = $emul->new_user_object($classname, [], true);
     foreach($class_obj->properties as &$property){
