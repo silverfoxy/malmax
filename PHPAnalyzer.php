74a5d77f6bace53539cb7a4f72bef445a0b8a664
Babak AminAzad <baminazad@cs.stonybrook.edu>
2022-06-25 15:59:43 -0400

    Fix current line when a function is called as part of condition

--- PHPAnalyzer.php
+++ PHPAnalyzer.php
@@ -892,7 +892,7 @@ class PHPAnalyzer extends \PHPEmul\OOEmulator
                 if (!$break && is_array($node->elseifs) && sizeof($node->elseifs) > 0) {
                     // Main branch is not satisfied, now checking the elseif branches
                     foreach ($node->elseifs as $elseif) {
-                        $this->current_line = $node->getLine();
+                        $this->current_line = $elseif->cond->getLine();
                         $this->lineLogger->logNodeCoverage($elseif->cond, $this->current_file);
                         $branch_condition = $this->evaluate_expression($elseif->cond);
                         $branch_conditions[$this->statement_id($elseif)] = $branch_condition;
