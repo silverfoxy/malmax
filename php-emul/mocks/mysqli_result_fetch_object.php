006cfe420e9f2abf47ebc9dfee9bee196c20efd3
Babak AminAzad <baminazad@cs.stonybrook.edu>
2022-09-05 13:12:58 -0400

    Preserve the type of existing properties in mysqli_result_fetch_object

--- php-emul/mocks/mysqli_result_fetch_object.php
+++ php-emul/mocks/mysqli_result_fetch_object.php
@@ -5,8 +5,13 @@ use PHPEmul\OOEmulator;
 function mysqli_result_fetch_object_mock($emul, $class, $constructor_args = []){
     $classname = strtolower($class->value->value);
     $class_obj = $emul->new_user_object($classname, [], true);
-    foreach($class_obj->properties as &$property){
-        $property = new \PHPEmul\SymbolicVariable();
+    foreach($class_obj->properties as $property_name => $property_value) {
+        if ($property_value instanceof \PHPEmul\EmulatorObject) {
+            $class_obj->properties[$property_name] = new \PHPEmul\SymbolicVariable($property_name, '*', \PhpParser\Node\Stmt\ClassLike::class, true, [$property_value], $property_value->classname);
+        }
+        else {
+            $class_obj->properties[$property_name] = new \PHPEmul\SymbolicVariable();
+        }
     }
     return $class_obj;
 }
